2c60ae376ba33008d552d6e489f96a9ceed7ed78
Sohyeon Kim <ksh2904@g.skku.edu>
2019-01-04 17:45:12 +0900

    Add detail in H-index function

--- fainsa_function.R
+++ fainsa_function.R
@@ -1,10 +1,9 @@
-
-## find H-index
-
 H_index <- function(num_cite) {
-  num_cite <- sort(num_cite, decreasing = TRUE) ## integer vector of the number of citation
+  # num_cite is integer vector of the number of citation
+  # returns H-index of a researcher
+  num_cite <- sort(num_cite, decreasing = TRUE) 
   ranking <- 1:length(num_cite)
-  
+  #check weather H-index is 0
   if(num_cite[1] == 0) {
     return(0)
   }
